d7661386802b568deb246222337af41577314973
ddas4548 <ddas4548@gmail.com>
2025-08-03 12:36:00 +0530

    implement customQuery hooks

--- 11-api-handling/frontend/src/App.jsx
+++ 11-api-handling/frontend/src/App.jsx
@@ -1,42 +1,21 @@
+import "./App.css";
 
-import { useState } from 'react'
-import './App.css'
-import { useEffect } from 'react'
-import axios from 'axios'
+import useCustomQuery from "./hooks/customQuery";
 function App() {
- 
-  const [products, setProducts] = useState([])
-  const [error,setError]= useState(false)
-  const [loading, setLoading] = useState(false)
-  useEffect(()=>{
-    (async()=> {
-      try {
-        setError(false)
-        setLoading(true)
-        const response = await axios.get("/api/products")
-        console.log(response.data)
-        setProducts(response.data)
-        setLoading(false)
-      } catch (error) {
-        console.log(error)
-        setError(true)
-        setLoading(false)
-      }
-    })()
+  const { products, error, loading } = useCustomQuery("/api/products");
 
-  },[])
-  if(error){
-    return<h1>Something went wrong</h1>
+  if (error) {
+    return <h1>Something went wrong</h1>;
   }
-  if (loading){
-    return <h1>Loading....</h1>
+  if (loading) {
+    return <h1>Loading....</h1>;
   }
   return (
-   <>
-   <h1>hello world</h1>
-   <h2>Number of products : {products.length}</h2>
-   </>
-  )
+    <>
+      <h1>hello world</h1>
+      <h2>Number of products : {products.length}</h2>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ 11-api-handling/frontend/src/hooks/customQuery.js
@@ -0,0 +1,33 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useState } from "react"
+
+const useCustomQuery =(urlPath)=>{
+const [products, setProducts] = useState([])
+  const [error,setError]= useState(false)
+  const [loading, setLoading] = useState(false)
+  useEffect(()=>{
+    (async()=> {
+      try {
+        setError(false)
+        setLoading(true)
+        const response = await axios.get(urlPath)
+        console.log(response.data)
+        setProducts(response.data)
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
+        setError(true)
+        setLoading(false)
+      }
+    })()
+
+  },[])
+  return {
+    products,
+    error,
+    loading,
+  }
+}
+
+export default useCustomQuery;
