0b9617976528d4ca7fbd5f310bd0a4cd572d2b06
ddas4548 <ddas4548@gmail.com>
2025-08-03 12:20:34 +0530

    loading and error state is done

--- 11-api-handling/frontend/src/App.jsx
+++ 11-api-handling/frontend/src/App.jsx
@@ -6,16 +6,31 @@ import axios from 'axios'
 function App() {
  
   const [products, setProducts] = useState([])
-
+  const [error,setError]= useState(false)
+  const [loading, setLoading] = useState(false)
   useEffect(()=>{
     (async()=> {
-      const response = await axios.get("/api/products")
-      console.log(response.data)
-      setProducts(response.data)
+      try {
+        setError(false)
+        setLoading(true)
+        const response = await axios.get("/api/products")
+        console.log(response.data)
+        setProducts(response.data)
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
+        setError(true)
+        setLoading(false)
+      }
     })()
 
   },[])
-
+  if(error){
+    return<h1>Something went wrong</h1>
+  }
+  if (loading){
+    return <h1>Loading....</h1>
+  }
   return (
    <>
    <h1>hello world</h1>
